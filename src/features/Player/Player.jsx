7f64cf2a17cd09180077e32ea4eb008de1353429
ernestoacosta75 <ernestoacostacuba@yahoo.com.mx>
2025-06-01 16:34:06 +0200

    Using  useRef hook to  connecting & accessing HTML elements

--- src/features/Player/Player.jsx
+++ src/features/Player/Player.jsx
@@ -1,23 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
+  const playerName = useRef("unknown entity");
   const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-  
-  const handleChange = (event) => {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  };
 
   const handleClick = () => {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
   };
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" value={enteredPlayerName} onChange={handleChange} />
+        <input ref={playerName} type="text"/>
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
